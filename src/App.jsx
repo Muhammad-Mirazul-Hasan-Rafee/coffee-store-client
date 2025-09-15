0e7c91880fa56bc1963300b80f898f843e96b3c8
Rafee <rafeehasan88@gmail.com>
2025-09-15 21:17:04 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './app.css';
+
+
+const App = () => {
+    return (
+        <div>
+            <h2 className='h-4 text-center'>Coding restarted Now</h2>
+        </div>
+    );
+};
+
+export default App;
