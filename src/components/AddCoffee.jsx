471d177a03f90e06b60c6f7f7860503eaa17ff7e
Rafee <rafeehasan88@gmail.com>
2025-10-15 06:50:00 +0600

    post

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,23 +1,56 @@
+import Swal from "sweetalert2";
+import CoffeeIcon from '../assets/images/icons/coffee.png';
+
 const AddCoffee = () => {
-    const handleAddCoffee = (event) =>{
-        event.preventDefault();
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
 
-        const form = event.target;
-        const name = form.name.value;
-        const quantity = form.quantity.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photo = form.photo.value;
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
 
-        const newCoffee = {name , quantity , supplier ,taste , category , details , photo};
-        console.log(newCoffee);
-    
-    }
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+    console.log(newCoffee);
+
+    // send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: '☕Coffee inserted successfully!',
+            icon: "success",
+            draggable: true,
+          });
+        }
+      });
+  };
+
   return (
     <div className="bg-[#F4F3F0] p-24">
-      <h2 className="font-extrabold text-3xl">Add a Coffee</h2>
+      <h2 className="font-extrabold text-3xl text-neutral-800 text-center">
+        Add your Coffee
+      </h2>
       <form onSubmit={handleAddCoffee}>
         {/* form name and quantity row */}
         <div className="md:flex mb-4">
@@ -26,9 +59,9 @@ const AddCoffee = () => {
               <span className="label-text">Coffee Name</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="name"
+                type="text"
+                name="name"
                 placeholder="Coffee name"
                 className="input input-bordered w-full"
               />
@@ -39,9 +72,9 @@ const AddCoffee = () => {
               <span className="label-text">Available Quantity</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="quantity"
+                type="text"
+                name="quantity"
                 placeholder="Available Quantity"
                 className="input input-bordered w-full"
               />
@@ -55,9 +88,9 @@ const AddCoffee = () => {
               <span className="label-text">Supplier Name</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="supplier"
+                type="text"
+                name="supplier"
                 placeholder="Supplier name"
                 className="input input-bordered w-full"
               />
@@ -68,9 +101,9 @@ const AddCoffee = () => {
               <span className="label-text">Available Quantity</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="taste"
+                type="text"
+                name="taste"
                 placeholder="Taste"
                 className="input input-bordered w-full"
               />
@@ -84,10 +117,10 @@ const AddCoffee = () => {
               <span className="label-text">Category</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="category"
-                placeholder="Coffee name"
+                type="text"
+                name="category"
+                placeholder="Coffee Category "
                 className="input input-bordered w-full"
               />
             </label>
@@ -97,10 +130,10 @@ const AddCoffee = () => {
               <span className="label-text">Details</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="details"
-                placeholder="Available Quantity"
+                type="text"
+                name="details"
+                placeholder="Details"
                 className="input input-bordered w-full"
               />
             </label>
@@ -113,17 +146,21 @@ const AddCoffee = () => {
               <span className="label-text">Photo URL</span>
             </label>
             <label className="input-group">
-          
               <input
-                type="text" name="photo"
+                type="text"
+                name="photo"
                 placeholder="Photo URL"
                 className="input input-bordered w-full"
               />
             </label>
           </div>
         </div>
-       
-        <input type="submit" value="Add Coffee" className="btn justify-center" />
+
+        <input
+          type="submit"
+          value="Add Coffee"
+          className="btn justify-center hover:bg-blue-950 hover:text-white"
+        />
       </form>
     </div>
   );
