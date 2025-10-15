44702541dd04f27aca816cbf5e09149230717340
Rafee <rafeehasan88@gmail.com>
2025-10-15 21:17:54 +0600

    coffee count added useLoader Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useLoaderData } from 'react-router-dom';
 import './app.css';
 
 
 const App = () => {
+
+    const coffees = useLoaderData();
+
+
+
+
     return (
         <div>
             <h2 className='h-4 text-center'>Coding restarted Now</h2>
+            <p className='text-center'>Total coffee order {coffees.length}</p>
         </div>
     );
 };
